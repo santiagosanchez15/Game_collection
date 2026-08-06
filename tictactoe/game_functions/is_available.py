cd2a0dc2f22e43d454a780f746b6afe6dd8cc672
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-08-06 09:56:13 -0700

    all game functions added need to add tie i think

---
+++ tictactoe/game_functions/is_available.py
@@ -0,0 +1,4 @@
+
+def is_available(array_game:list[str], space:int)->bool:
+    '''Returns True if place is empty if not then False'''
+    return array_game[space] == "_"
---
+++ tictactoe/game_functions/print_board.py
@@ -0,0 +1,13 @@
+
+def print_board(array_game:list[str]):
+    '''Prints boards game'''
+
+    for i in range(len(array_game)):
+        print(array_game[i] , end="")
+        
+        if i % 3 != 2:
+            print("|",end="")
+        else:
+            print()
+            if i != len(array) - 1:
+                print("______")
