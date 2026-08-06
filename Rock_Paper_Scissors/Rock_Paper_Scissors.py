879a1cf57eeb90c9d78fb86ea28cf2a031c9bc71
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-08-05 22:34:42 -0700

    rock paper sicsosrs game finished, need to fix the user class but first i will add all the games

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -3,12 +3,15 @@ from results_dict import results
 def game(user1_choice:str, npc_choice:str) -> bool:
     '''Checks options, returns winner| True-> User Wins| False->NPC wins, -1 for draw'''
 
-    
     user_outcome:str = results[user1_choice][npc_choice]#get result
 
     if user_outcome == "win": #Player wins
+        print("You WIN!!! ")
         return True
     elif user_outcome == "lose":# player loses
+        print("NPC WINS and You LOSE!!!")
         return False
+
+    print("There is a DRAW")
     return -1 #Draw
 
