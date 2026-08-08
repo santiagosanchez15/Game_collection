c7d65fd6bad5f3bf0ea561af3af606de99a956fa
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-08-08 13:51:46 -0700

    some changes to winner functions, now to focus on the minimax algorithm, credits to the coding train who is my teacher right now

--- tictactoe/npc/minimax.py
+++ tictactoe/npc/minimax.py
@@ -3,16 +3,17 @@ import os
 import math
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from game_functions.is_winner import is_winner
+from game_functions.is_winner import check_winner
 
 score_games:dict[bool|int:int] = {
-    True: 1,
-    False: -1,
-    -1: 0
+    'O': 1,
+    'X': -1,
+    'tie': 0
 }
 
 def minimax(board:list[str], depth: int, is_maximizing) -> int:
     '''Minimax algorithm to return score'''
-    if is_winner(board, 'O'):
-        return 1
+    winner = check_winner(board)
+    if winner != False:
+        return score_games[winner]
     pass
