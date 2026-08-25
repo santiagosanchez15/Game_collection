d381a3f1f5a544f0ab4fa8581992e7983c167544
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-08-25 09:27:56 -0700

    initial additions to the main game with pseduocode almost done, need to make changes to the user

---
+++ blackjack/user/blackjack.py
@@ -0,0 +1,43 @@
+from userObject import User
+
+def blackjack(user: User, house: House): #pseudocode for balckjack game
+    '''BlackJack game
+    True -> User wins
+    False -> The House wins | tied
+
+    '''
+
+    #Get initial Draw for the user
+    user.Intial_Draw()
+    house.Initial_Draw()
+
+    if user.is_over21() is True:
+        print("The house wins, you lost!")
+        return False
+    if house.is_over21() is True:
+        print("You won! Congratulations!!")
+        return True
+
+    while True: 
+
+        #check if any of the players wants another card
+        user_input : bool =user.get_Card()
+        house_input : bool = house.get_Card()
+
+        #FIXME need a function to recalcualte all the ace in the deck Maybe a dict that has a count of the number of card like {"A": 1, "2": 0, } value given by that
+        
+
+        #Check if any of the players is 21
+        if user.is_over21():
+            print(f"The house wins, you lost, you are over 21, the house got {house.Get_total()}!")
+            return False
+
+        if house.is_over21():
+            print(f"You won the House is over 21! Congratulations!!")
+            return True
+
+        #Check if None of the players wants to keep playing
+        if user_input is False and house_input is False:
+            return user.check_winner(house)
+
+
--- blackjack/user/userObject.py
+++ blackjack/user/userObject.py
@@ -11,25 +11,93 @@ class User(Player):
 
         super().__init__(name=name)
 
-    def get_Card(self):
-        '''Get a random card'''
+    def Print_Cards(self):
+        print("The cards in your deck are:\n")
+        for card in self.deck:
+            print(f"  -{card}")
 
-        card : str = choice(self.FullDeck)
+    def Intial_Draw(self):
+        '''Initial Draw for the User'''
+        self.deck.append(choice(self.FullDeck)) #Get first Card
+        self.deck.append(choice(self.FullDeck)) #Get Second Card
+        self.Print_Cards()
+        
+
+    def Calculate_total_deck(self):
+        '''Add value of card to the total of my deck'''
+        self.ValueDeck = sum(self.ValueCards[card] for card in self.deck)  # sums all values of cards in deck #FIXME test that it works
+
+    #Get Functions below
+
+    def get_Card(self) -> bool:
+        '''Get a random card if input is yes'''
+
+        user_input : bool = self.get_yes_no_input() #ask if the user wants one more card 
+        if user_input is False:
+            return False #User didnt want another card
+        
+        card : str = choice(self.FullDeck) #Get a random card
+
+        if self.Is_Ace(card): #Check if card is ace
+            card = self.choose_value_ace() #Get value for Ace
+        self.Calculate_total_deck(card) #Add value to the value of deck
         self.deck.append(card)
+        return True #user wanted another card
+
+    def Get_Total(self) -> int:
+        '''Return total amount of cards'''
+        return self.ValueDeck
+
+    def get_yes_no_input(self) -> bool:
+
+        user_input = input("Do you want another card. yes|no  ")
+        clean_input = user_input.strip().lower() # clean input and lower the cases if upper cases
+        
+        try: #check if it falls in one of the cases otherwise call the function again
+            if clean_input == "yes":
+                return True
+            if clean_input == "no":
+                return False
+    
+            raise ValueError("Wrong input please try again| yes or no only\n")
+    
+        except ValueError as e: #Wrong input, call the function again
+            print(e)
+            return self.get_input()
+
+    #Ace functions below
+
+    def choose_value_ace(self) -> str:
+        '''Player chose value ace'''
+        print(f"You got and Ace! Your current total is: {self.ValueDeck}\n")
+        user_input : str= input("Do you want your card to be 1 or 11").lower().strip()
+        self.is_valid_ace_value(user_input)
+        return user_input
+
+    def is_valid_ace_value(self, user_input: str):
+        '''Check if input is valid ace value'''
+
+        try: 
+            if user_input != "1" or user_input != "11":
+                raise ValueError
+            return 
+
+        except ValueError:
+            print("Wrong input please try again| Only 1 or 21")
+            return self.choose_value_ace()
 
     def Is_Ace(self, card:str) -> bool:
-        '''Check if player got an ace, if so retrun true else no '''
-        return card == "A"
-
-
+            '''Check if player got an ace, if so retrun true else no '''
+            return card == "A"
+    
+    
     def num_ace_inDeck(self) -> int:
         '''Returns number of Ace in deck'''
         num_ace : int = self.deck.count("A") #Get num ace in deck
         print(f"You have {num_ace} Ace in your deck")
         return num_ace
 
-    def Get_Total(self):
-        return sum(lambda card : self.ValueCards[card] for card in self.deck) # sums all values of cards in deck #FIXME test that it works
+    
 
 
 
